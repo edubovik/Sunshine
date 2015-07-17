bf0e1151cb003046dc6aee5abce4c998e457e364
Eugene Dubovik <edubovik90@gmail.com>
2015-07-17 18:43:25 +0300

    added stringFormat to temperature

--- app/src/main/java/ua/sunshine/dubovik/ForecastAdapter.java
+++ app/src/main/java/ua/sunshine/dubovik/ForecastAdapter.java
@@ -39,7 +39,7 @@ public class ForecastAdapter extends CursorAdapter {
      */
     private String formatHighLows(double high, double low) {
         boolean isMetric = Utility.isMetric(mContext);
-        return Utility.formatTemperature(high, isMetric) + "/" + Utility.formatTemperature(low, isMetric);
+        return Utility.formatTemperature(mContext, high, isMetric) + "/" + Utility.formatTemperature(mContext,low, isMetric);
     }
 
     /*
@@ -99,10 +99,10 @@ public class ForecastAdapter extends CursorAdapter {
 
         // Read high temperature from cursor
         double high = cursor.getDouble(ForecastFragment.COL_WEATHER_MAX_TEMP);
-        viewHolder.highTempView.setText(Utility.formatTemperature(high, isMetric));
+        viewHolder.highTempView.setText(Utility.formatTemperature(context, high, isMetric));
 
         double low = cursor.getDouble(ForecastFragment.COL_WEATHER_MIN_TEMP);
-        viewHolder.lowTempView.setText(Utility.formatTemperature(low, isMetric));
+        viewHolder.lowTempView.setText(Utility.formatTemperature(context, low, isMetric));
     }
 
     public static class ViewHolder {
